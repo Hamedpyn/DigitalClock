25d67ff8b7e8091c26d165489872263040e3095e
Hamed <hamedpyn@gmail.com>
2023-08-12 13:26:02 +0430

    get the time and put it in html ( js )

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,15 @@
 let hour = document.querySelector('#hour'),
     minutes = document.querySelector('#minute'),
     seconds = document.querySelector('#seconds');
+
+setInterval(() => {
+    // get every hour , minute and second 
+    let nowHour = new Date().getHours()
+    let nowMinute = new Date().getMinutes()
+    let nowSecond = new Date().getSeconds()
+
+    // save them in their html element
+    hour.innerHTML = nowHour
+    minutes.innerHTML = nowMinute
+    seconds.innerHTML = nowSecond
+}, 1000);

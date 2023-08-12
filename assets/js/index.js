2c29297010e51b2e4471f891e51aa9b38b88dc4b
Hamed <hamedpyn@gmail.com>
2023-08-12 13:27:34 +0430

    if it was less than 10 , add a 0 besides it

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,6 +10,15 @@ setInterval(() => {
     let nowMinute = new Date().getMinutes()
     let nowSecond = new Date().getSeconds()
 
+    // if it was less than 10 , add a 0 besides it ( 01 )
+    if (nowHour < '10') {
+        nowHour = '0' + nowHour
+    } else if (nowMinute < '10') {
+        nowMinute = '0' + nowMinute
+    } else if (nowSecond < '10') {
+        nowSecond = '0' + nowSecond
+    }
+
     // save them in their html element
     hour.innerHTML = nowHour
     minutes.innerHTML = nowMinute
